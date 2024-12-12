79237fc8f25e5dde64abcca6a7110d1545bce3e2
John Brennan McAllister <143151828+JohnBMcAllister@users.noreply.github.com>
2024-12-12 12:52:30 -0600

    solution to problem 1

--- Day_3/Problem_1/main.py
+++ Day_3/Problem_1/main.py
@@ -1,8 +1,26 @@
 import re
 
+sequence = []
+result = []
+combined = 0
+
 def find_Mull_Occurances(sequence):
     pattern = r'mul\((\d{1,3}),(\d{1,3})\)'
     matches = re.findall(pattern, sequence)
+    print("Pairs: ", matches)
 
-    return matches
+    return [(int(a) * int(b)) for a, b in matches]
+
+with open('input.txt', 'r') as file:
+    for line in file:
+        sequence.append(line)
+
+for line in sequence:
+    result.append(find_Mull_Occurances(line))
+
+for x in result:
+    for y in x:
+        combined += y
+
+print (combined)
 
