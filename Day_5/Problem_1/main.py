a52100d726ca3a68357e0ec3a478dc45a68d8e82
John Brennan McAllister <143151828+JohnBMcAllister@users.noreply.github.com>
2024-12-19 15:26:49 -0600

    problem 5 doesnt work

--- Day_5/Problem_1/main.py
+++ Day_5/Problem_1/main.py
@@ -1,5 +1,6 @@
 page_rules = {}
 manuals = []
+count = 0
 
 with open("input.txt", "r") as f:
     for line in f:
@@ -12,4 +13,23 @@ with open("input.txt", "r") as f:
                 page_rules[x] = []
             page_rules[x].append(y)
 
-print(page_rules)
+        if "," in line:
+            sequence = [int(x) for x in line.split(",")]
+            manuals.append(sequence)
+
+for x in range(len(manuals)):
+    for y in range(len(manuals[x])):
+        if y == 0:
+            continue
+        cur = manuals[x][y]
+        prev = manuals[x][:y]
+
+        for prev in prev:
+            if cur in page_rules and prev in page_rules[cur]:
+                continue
+            else:
+                count += 1
+
+print("page rules: ", page_rules)
+print("manuals: ", manuals)
+print("count: ", count)
